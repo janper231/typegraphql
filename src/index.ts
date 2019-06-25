cd4292c98227089dd596cc57e25d6272f1b00406
Jaen Carlos Aponte <jaencarlosaponte@MacBook-Air-de-Jaen.local>
2019-06-24 21:18:16 -0500

    inicio de creacion de api con typegraphql

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import "reflect-metadata";
+import { ApolloServer } from 'apollo-server-express';
+import * as Express from 'express';
+import { buildSchema, Resolver, Query } from 'type-graphql';
+
+@Resolver()
+class HelloResolver {
+	@Query(() => String)
+	async Hello() {
+		return 'Hello world';
+	}
+}
+const main = async () => {
+	const schema = await buildSchema({
+		resolvers: [ HelloResolver ]
+	});
+	const apolloserver = new ApolloServer({ schema });
+	const app = Express();
+    apolloserver.applyMiddleware({ app });
+    app.listen(4000, () =>{
+        console.log("server started on http://:localhost:4000")
+    });
+};
+main();
